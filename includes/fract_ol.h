adab8e8398eafb2ed8c378368dee98ab201628b8
Vince Richese <vrichese@oa-o1.21-school.ru>
2019-07-16 21:03:41 +0300

    Not enough knowledge about fractol, so i start reading some books

--- includes/fract_ol.h
+++ includes/fract_ol.h
@@ -6,7 +6,7 @@
 /*   By: vrichese <vrichese@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/15 15:33:31 by vrichese          #+#    #+#             */
-/*   Updated: 2019/07/15 16:26:56 by vrichese         ###   ########.fr       */
+/*   Updated: 2019/07/16 20:55:42 by vrichese         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,26 @@
 # include "ft_printf.h"
 # include "get_next_line.h"
 # include "libft.h"
+# include <mlx.h>
+
+/*
+**	In honor of the great mathematician who discovered the world of fractals
+**	i've named my struct by his name(Benoit Mandelbrot)
+*/
+
+typedef struct		s_window
+{
+	unsigned int	width;
+	unsigned int	height;
+}					t_window;
+
+typedef struct		s_benoit
+{
+	void			*connect;
+	void			*window;
+	void			*picture;
+	void			*pixels;
+	t_window		win;
+}					t_benoit;
 
 #endif
